fedf2e323163134357c37d70861f821b2926eff9
M Sadiq Warsi <157520386+msw2005@users.noreply.github.com>
2026-02-01 13:50:46 +0000

    Add comment marker in asgi.py

--- Underground-Route-Planner-main/underground_project/asgi.py
+++ Underground-Route-Planner-main/underground_project/asgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.asgi import get_asgi_application
-
+#CA
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'underground_project.settings')
 application = get_asgi_application()
